144371a22840ab6e6918bb6c4b69de915fd1fe52
jyi <joshua.yi@amadeus.com>
2018-07-25 18:17:08 -0400

    Lift Grid state up

--- react-client/src/Components/Grid/Grid.jsx
+++ react-client/src/Components/Grid/Grid.jsx
@@ -2,33 +2,15 @@ import React, { Component } from 'react';
 import Row from './Row/Row';
 
 class Grid extends Component {
+
     constructor(props) {
         super(props);
-        this.state = {
-            rows: []
-        }
     }
 
-    componentDidMount() {
-
-        // Get main grid data
-        fetch("https://api.coinmarketcap.com/v2/ticker/", {
-            method: "GET"
-        })
-            .then((response) => response.json())
-            .then(
-                // Success
-                (response) => {
-                    let rows = createRows(response);
-
-                    this.setState({rows: rows});
-                    //console.log("state", this.state.rows)
-                },
-                // Failure
-                (reason) => { console.log(reason) });
-    }
 
     render() {
+        //let rows = this.props.data.map((coin) => (<Row data={coin} />));
+
         return (
             <div class="row">
                 <div class="col-xs-12">
@@ -45,7 +27,13 @@ class Grid extends Component {
                         </tr>
                         </thead>
                         <tbody>
-                        {this.state.rows}
+                            {
+                                /*{rows}*/
+                                this.props.data.map((coin) => (
+                                    <Row data={coin} />
+                                ))
+                            }
+
                         </tbody>
                     </table>
                 </div>
@@ -54,44 +42,6 @@ class Grid extends Component {
     }
 
 
-}
-
-/**
- * Compare coin ranks. The lowest rank is shown first in the list
- * @param coin1
- * @param coin2
- * @returns {number}
- */
-function compareCoins(coin1, coin2) {
-    if (coin1.rank < coin2.rank) {
-        return -1;
-    }
-
-    if (coin1.rank > coin2.rank) {
-        return 1;
-    }
-
-    return 0;
-}
-
-/**
- * We create Row objects from the object that is returned by the
- * CoinMarketCap API
- *
- * @param response
- * @returns {any[]}
- */
-function createRows(response) {
-    // Change to array first
-    let coins = Object.keys(response.data).map(function (key) {
-        return response.data[key];
-    });
-
-    // Sort array by rank of Coin
-    coins.sort(compareCoins);
-
-    // Create grid rows for body
-    return coins.map((coin) => (<Row data={coin} />));
 }
 
 export default Grid;
