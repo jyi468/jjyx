14caf9cf8126613fcffb0f45f0353039b517148b
Josh <jyi468@gmail.com>
2018-07-26 09:20:37 -0400

    Fix header

--- react-client/src/Components/Header/NavBar.jsx
+++ react-client/src/Components/Header/NavBar.jsx
@@ -10,44 +10,46 @@ function NavBar() {
             </button>
 
             <div class="collapse navbar-collapse" id="navbarContent">
-                <li className="nav-item dropdown" id="marketCap">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Market Cap
+                <ul class="navbar-nav">
+                    <li className="nav-item dropdown" id="marketCap">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Market Cap
                         </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a className="dropdown-item" href="#">All Cryptocurrencies</a>
-                        <a className="dropdown-item" href="#">Coins Only</a>
-                        <a className="dropdown-item" href="#">Tokens Only</a>
-                    </div>
-                </li>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a className="dropdown-item" href="#">All Cryptocurrencies</a>
+                            <a className="dropdown-item" href="#">Coins Only</a>
+                            <a className="dropdown-item" href="#">Tokens Only</a>
+                        </div>
+                    </li>
 
-                <li className="nav-item dropdown" id="tradeVolume">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Trade Volume
+                    <li className="nav-item dropdown" id="tradeVolume">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Trade Volume
                         </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    </div>
-                </li>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        </div>
+                    </li>
 
-                <li className="nav-item dropdown" id="trending">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Trending
+                    <li className="nav-item dropdown" id="trending">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Trending
                         </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    </div>
-                </li>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        </div>
+                    </li>
 
-                <li className="nav-item dropdown" id="tools">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Tools
+                    <li className="nav-item dropdown" id="tools">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Tools
                         </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    </div>
-                </li>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        </div>
+                    </li>
+                </ul>
             </div>
         </nav>
     );
