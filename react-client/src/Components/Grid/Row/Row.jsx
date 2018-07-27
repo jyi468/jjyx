d682199ff990fe555af5db03fa1c2e53e205f412
Josh <jyi468@gmail.com>
2018-07-27 09:28:49 -0400

    Add link

--- react-client/src/Components/Grid/Row/Row.jsx
+++ react-client/src/Components/Grid/Row/Row.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Route, Redirect, Link } from 'react-router-dom';
 
 function Row(props) {
     let data = props.data;
+    let linkUrl = '/currencies/' + data.name;
     return (
         <tr key={data.rank}>
             <th scope="row">{data.rank}</th>
-            <td>{data.name}</td>
+            <td>
+                <Link to={linkUrl}>{data.name}</Link>
+            </td>
             <td>{data.quotes.USD.market_cap}</td>
             <td>{data.quotes.USD.price}</td>
             <td>{data.quotes.USD.volume_24h}</td>
