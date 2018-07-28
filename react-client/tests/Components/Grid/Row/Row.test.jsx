3af91ea6d6162e3d6f17c5b33a0fca1a7addaeca
jyi <joshua.yi@amadeus.com>
2018-07-28 15:37:59 -0400

    Add Row test

---
+++ react-client/tests/Components/Grid/Row/Row.test.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
+
+import Row from '../../../../src/Components/Grid/Row/Row';
+describe('Row', () => {
+    it('should render correctly', () => {
+        const coinData = {
+            "name": 'Bitcoin',
+            "rank": 1,
+            "circulating_supply": 17170100.0,
+            "total_supply": 17170100.0,
+            "max_supply": 21000000.0,
+            "quotes": {
+                "USD": {
+                    "price": 8190.56,
+                    "volume_24h": 5954760000.0,
+                    "market_cap": 140632734256.0,
+                    "percent_change_1h": -0.26,
+                    "percent_change_24h": 0.04,
+                    "percent_change_7d": 10.42
+                }
+            }
+        };
+
+        const output = shallow(
+            <Row data={coinData} />
+        );
+
+        expect(shallowToJson(output)).toMatchSnapshot();
+    });
+});
