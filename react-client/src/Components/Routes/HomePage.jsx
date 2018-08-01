824ddd18638a680f2fea1fe8014304ef52442a6d
jyi <joshua.yi@amadeus.com>
2018-08-01 18:19:55 -0400

    Clean up comments

--- react-client/src/Components/Routes/HomePage.jsx
+++ react-client/src/Components/Routes/HomePage.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import GridContainer from "../Grid/GridContainer";
 import NavBar from "../Header/NavBar"
-import GridUtils from "../Grid/GridUtils";
 import { fetchGrid } from "../../Redux/actions";
 
 // Order: constructor(), static getDerivedStateFromProps(), render(), componentDidMount()
@@ -10,22 +9,10 @@ class HomePage extends Component {
     // Lifecycle methods
     constructor(props) {
         super(props);
-        // TODO: Create nested models here with main grid, pagination, tabs, etc.
-        /*this.state = {
-            maxPage: 17,
-            coins: []
-        }*/
     }
 
     // TODO: Pass in gridData, pageData or use nested model?
     render() {
-        /*const pageNumber = parseInt(this.props.match.params.pageNumber) || 1;
-        return (
-            <div className="container main-section">
-                <NavBar />
-                <Grid data={this.state.coins} pageNumber={pageNumber} maxPage={this.state.maxPage}/>
-            </div>
-        )*/
         return (
             <div className="container main-section">
                 <NavBar />
@@ -45,35 +32,9 @@ class HomePage extends Component {
             this.props.dispatch(fetchGrid(newPageNumber, 100));
         }
     }
-
-    // Other methods
-    /*fetchData(pageNumber) {
-        // Get main grid data
-        fetch("https://api.coinmarketcap.com/v2/ticker/?start=" + (100 * (pageNumber - 1) + 1) +"&limit=100", {
-            method: "GET"
-        })
-            .then((response) => response.json())
-            .then(
-                // Success
-                (response) => {
-                    // Replace with Redux
-
-
-                    /!*let maxPage = Math.ceil(response.metadata.num_cryptocurrencies / 100);
-                    let coins = GridUtils.createCoins(response);
-
-                    // Replace with Redux
-                    this.setState({
-                        maxPage: maxPage,
-                        coins: coins,
-                    });*!/
-                },
-                // Failure
-                (reason) => { console.log(reason) });
-    }*/
 }
 
-// Map state to props that will be passed to presentational ocmponent
+// Map state to props that will be passed to presentational component
 const mapStateToProps = state => {
     return {
         grid: state.grid
