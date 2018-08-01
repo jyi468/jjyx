c9e6aa1ce291e3244c574c67e234ffdf5e46e293
jyi <joshua.yi@amadeus.com>
2018-08-01 18:53:56 -0400

    Update Charts container

--- react-client/src/Components/Charts/ChartsContainer.jsx
+++ react-client/src/Components/Charts/ChartsContainer.jsx
@@ -2,11 +2,8 @@ import { connect } from 'react-redux';
 import Charts from './Charts';
 
 const mapStateToProps = state => ({
-    data: state.grid.coins,
-    pageNumber: state.grid.pageNumber,
-    maxPage: state.grid.maxPage
 });
 
 export default connect(
     mapStateToProps
-)(Grid)
+)(Charts)
