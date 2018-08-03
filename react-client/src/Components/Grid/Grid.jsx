9c83491ea06ddb6f91dd57fc6d37a370c1662b23
jyi <joshua.yi@amadeus.com>
2018-08-03 16:26:09 -0400

    Change grid to functional component

--- react-client/src/Components/Grid/Grid.jsx
+++ react-client/src/Components/Grid/Grid.jsx
@@ -1,53 +1,43 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Row from './Row/Row';
 import Pagination from './Pagination/Pagination';
 
-class Grid extends Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
+const Grid = ({pageNumber, maxPage, data}) => (
+    <div class="row">
+        <div class="col-xs-12">
             <div class="row">
-                <div class="col-xs-12">
-                    <div class="row">
-                        <div class="col-xs-4 offset-md-8">
-                            <Pagination pageNumber={this.props.pageNumber} maxPage={this.props.maxPage} />
-                        </div>
-                    </div>
-                    <table className="table">
-                        <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Market Cap</th>
-                            <th scope="col">Price</th>
-                            <th scope="col">Volume (24h)</th>
-                            <th scope="col">Circulating Supply</th>
-                            <th scope="col">Change (24h)</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                this.props.data.map((coin) => (
-                                    <Row data={coin} />
-                                ))
-                            }
-
-                        </tbody>
-                    </table>
-                    <div class="row">
-                        <div class="col-xs-4 offset-md-8">
-                            <Pagination pageNumber={this.props.pageNumber} maxPage={this.props.maxPage} />
-                        </div>
-                    </div>
+                <div class="col-xs-4 offset-md-8">
+                    <Pagination pageNumber={pageNumber} maxPage={maxPage}/>
                 </div>
             </div>
-        )
-    }
+            <table className="table">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Market Cap</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Volume (24h)</th>
+                    <th scope="col">Circulating Supply</th>
+                    <th scope="col">Change (24h)</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    data.map((coin) => (
+                        <Row data={coin}/>
+                    ))
+                }
 
-}
+                </tbody>
+            </table>
+            <div class="row">
+                <div class="col-xs-4 offset-md-8">
+                    <Pagination pageNumber={pageNumber} maxPage={maxPage}/>
+                </div>
+            </div>
+        </div>
+    </div>
+);
 
 export default Grid;
