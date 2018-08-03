1d0b7e646718fb83894a169ebbb8acd54cc22fd6
jyi <joshua.yi@amadeus.com>
2018-08-03 17:02:29 -0400

    Refactor

--- react-client/src/Components/Grid/Pagination/Pagination.jsx
+++ react-client/src/Components/Grid/Pagination/Pagination.jsx
@@ -1,68 +1,58 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-class Pagination extends Component {
-    constructor(props) {
-        super(props);
-    }
+const Pagination = ({pageNumber}) => {
+    let next100 = (
+        <li class="page-item">
+            <Link to={`/${pageNumber + 1}`} class="page-link">
+                Next 100 →
+            </Link>
+        </li>
+    );
 
-    //handleButtonClick()
+    let prev100 = (
+        <li class="page-item">
+            <Link to={`/${pageNumber - 1}`} class="page-link">
+                ← Previous 100
+            </Link>
+        </li>
+    );
 
-    render() {
-        const pageNumber = this.props.pageNumber;
-
-        let next100 = (
-            <li class="page-item">
-                <Link to={`/${pageNumber + 1}`} class="page-link">
-                    Next 100 →
-                </Link>
-            </li>
-        );
-
-        let prev100 = (
-            <li class="page-item">
-                <Link to={`/${pageNumber - 1}`} class="page-link">
-                    ← Previous 100
-                </Link>
-            </li>
-        );
-
-        let viewAll = (
-            <li class="page-item">
-                <a class="page-link" href="all">View All</a>
-            </li>
-        );
+    let viewAll = (
+        <li class="page-item">
+            <a class="page-link" href="all">View All</a>
+        </li>
+    );
 
 
-        if (pageNumber === 1) {
-            // If page number 1, show Next 100
-            return (
-                <ul class="pagination">
-                    {next100}
-                    {viewAll}
-                </ul>
-            );
-        } else if (pageNumber === this.props.maxPage) {
-            // If page number n, show Previous 100
-            return (
-                <ul class="pagination">
-                    {prev100}
-                    {viewAll}
-                </ul>
-            );
-        }
-
-        // Else show all buttons
+    if (pageNumber === 1) {
+        // If page number 1, show Next 100
         return (
             <ul class="pagination">
-                {prev100}
                 {next100}
                 {viewAll}
             </ul>
         );
+    } else if (pageNumber === this.props.maxPage) {
+        // If page number n, show Previous 100
+        return (
+            <ul class="pagination">
+                {prev100}
+                {viewAll}
+            </ul>
+        );
     }
-}
+
+    // Else show all buttons
+    return (
+        <ul class="pagination">
+            {prev100}
+            {next100}
+            {viewAll}
+        </ul>
+    );
+};
 
 Pagination.propTypes = {
     pageNumber: PropTypes.number.isRequired
--- react-client/src/Components/Grid/Row/Row.jsx
+++ react-client/src/Components/Grid/Row/Row.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import RowUtils from './RowUtils';
 
-const Row = ({data}) => {
-    let linkUrl = '/currencies/' + data.name.toLowerCase();
-    return (
-        <tr key={data.rank}>
-            <th scope="row">{data.rank}</th>
-            <td>
-                <Link to={linkUrl}>{data.name}</Link>
-            </td>
-            <td>{RowUtils.numToUsd(data.quotes.USD.market_cap)}</td>
-            <td>{RowUtils.numToUsd(data.quotes.USD.price)}</td>
-            <td>{RowUtils.numToUsdInteger(data.quotes.USD.volume_24h)}</td>
-            <td>{RowUtils.numToSeparatedDigits(data.circulating_supply + ' ' + data.symbol)}</td>
-            <td>{data.quotes.USD.percent_change_24h + '%'}</td>
-        </tr>
-    );
-};
+const Row = ({data}) => (
+    <tr key={data.rank}>
+        <th scope="row">{data.rank}</th>
+        <td>
+            <Link to={`/currencies/${data.name.toLowerCase()}`}>{data.name}</Link>
+        </td>
+        <td>{RowUtils.numToUsd(data.quotes.USD.market_cap)}</td>
+        <td>{RowUtils.numToUsd(data.quotes.USD.price)}</td>
+        <td>{RowUtils.numToUsdInteger(data.quotes.USD.volume_24h)}</td>
+        <td>{RowUtils.numToSeparatedDigits(data.circulating_supply + ' ' + data.symbol)}</td>
+        <td>{data.quotes.USD.percent_change_24h + '%'}</td>
+    </tr>
+);
 
 export default Row;
