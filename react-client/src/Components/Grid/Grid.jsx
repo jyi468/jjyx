0ad1d33c522ae3c4ead4397ae27b6e856cd2bae8
jyi <joshua.yi@amadeus.com>
2018-08-04 12:54:20 -0400

    Update components

--- react-client/src/Components/Grid/Grid.jsx
+++ react-client/src/Components/Grid/Grid.jsx
@@ -3,38 +3,46 @@ import Row from './Row/Row';
 import Pagination from './Pagination/Pagination';
 
 const Grid = ({pageNumber, maxPage, data}) => (
-    <div class="row">
-        <div class="col-xs-12">
-            <div class="row">
-                <div class="col-xs-4 offset-md-8">
-                    <Pagination pageNumber={pageNumber} maxPage={maxPage}/>
-                </div>
-            </div>
-            <table className="table">
-                <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Market Cap</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Volume (24h)</th>
-                    <th scope="col">Circulating Supply</th>
-                    <th scope="col">Change (24h)</th>
-                </tr>
-                </thead>
-                <tbody>
-                {
-                    data.map((coin) => (
-                        <Row data={coin}/>
-                    ))
-                }
+    <div>
+        <div class="page-header mt-5 mb-5">
+            <h1>Top 100 Cryptocurrencies By Market Capitalization</h1>
+        </div>
 
-                </tbody>
-            </table>
-            <div class="row">
-                <div class="col-xs-4 offset-md-8">
-                    <Pagination pageNumber={pageNumber} maxPage={maxPage}/>
-                </div>
+        <div className="row">
+            <div className="col-xs-4 offset-md-8">
+                <Pagination pageNumber={pageNumber} maxPage={maxPage}/>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col-xs-12">
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Market Cap</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Volume (24h)</th>
+                        <th scope="col">Circulating Supply</th>
+                        <th scope="col">Change (24h)</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        data.map((coin) => (
+                            <Row data={coin}/>
+                        ))
+                    }
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col-xs-4 offset-md-8">
+                <Pagination pageNumber={pageNumber} maxPage={maxPage}/>
             </div>
         </div>
     </div>
